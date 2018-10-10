62b8f285a85c9d1015ee60c5c6ea0e17ea503bfb
Ivo Calado <txithihausen@gmail.com>
2018-10-10 12:43:40 -0300

    Adiciona exemplo sobre tipos primitivos
    Signed-off-by: Ivo Calado <txithihausen@gmail.com>

---
+++ java/02-DeclaracaoVariaveis/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		byte b = 120; //-2^7  a 2^7 - 1
+		short s = 1300;//-2^15 a 2^15 - 1
+		char c = 'a';//0 a 2^16 - 1
+		int i = -10; //-2^31 a 2^31 - 1
+		long l = 12; //-2^63 a 2^63 - 1
+		float f = 10.3f; //-1.4024*10^37 a 3.4028 * 10^38
+		double d = 10.3;
+		boolean v = true; //true ou false
+		
+		System.out.println(b);
+	}
+}
